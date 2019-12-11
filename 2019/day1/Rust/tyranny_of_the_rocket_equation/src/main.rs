8a9eab75f89c1bdf037034ae2c34e58fe68e2933
Liam <will.leemans@gmail.com>
2019-12-11 14:45:01 +0100

    :sparkles: : Add  2019_1-2_Rust

--- 2019/day1/Rust/tyranny_of_the_rocket_equation/src/main.rs
+++ 2019/day1/Rust/tyranny_of_the_rocket_equation/src/main.rs
@@ -8,38 +8,44 @@ fn main() {
     println!("mass for all modules {}", sum_of_fuel);
 }
 
-fn calculate_fuel_for_mass(mass: i32) -> i32 {
-    return (mass / 3) - 2;
+fn calculate_fuel_for_mass_and_fuel(mass: i32) -> i32 {
+    let fuel = (mass / 3) - 2;
+
+    if fuel <= 0 {
+        return 0;
+    }
+
+    fuel + calculate_fuel_for_mass_and_fuel(fuel)
 }
 
 fn calculate_fuel_for_all_modules(modules_masses: Lines<'_>) -> i32 {
-    return modules_masses
+    modules_masses
         .map(|x| x.parse().unwrap())
-        .map(|x| calculate_fuel_for_mass(x))
-        .sum();
+        .map(|x| calculate_fuel_for_mass_and_fuel(x))
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::calculate_fuel_for_mass;
+    use crate::calculate_fuel_for_mass_and_fuel;
 
     #[test]
     fn simple_fuel_calculation() {
-        assert_eq!(calculate_fuel_for_mass(12), 2)
+        assert_eq!(2, calculate_fuel_for_mass_and_fuel(12))
     }
 
     #[test]
     fn simple_fuel_calculation_with_rounding() {
-        assert_eq!(calculate_fuel_for_mass(14), 2)
+        assert_eq!(2, calculate_fuel_for_mass_and_fuel(14))
     }
 
     #[test]
     fn intermediate_fuel_calculation_with_rounding() {
-        assert_eq!(calculate_fuel_for_mass(1969), 654)
+        assert_eq!(966, calculate_fuel_for_mass_and_fuel(1969))
     }
 
     #[test]
     fn fuel_calculation_with_rounding() {
-        assert_eq!(calculate_fuel_for_mass(1969), 654)
+        assert_eq!(50346, calculate_fuel_for_mass_and_fuel(100756))
     }
 }
