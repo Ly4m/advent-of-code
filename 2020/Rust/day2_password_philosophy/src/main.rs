2a957ccc7591790700ea5951d1a9852be04ba633
William Leemans <will.leemans@gmail.com>
2020-12-02 22:23:33 +0100

    :sparkles: 2020 Day 2 part II

--- 2020/Rust/day2_password_philosophy/src/main.rs
+++ 2020/Rust/day2_password_philosophy/src/main.rs
@@ -1,14 +1,27 @@
 use std::fs;
 
 fn main() {
-   let value =  fs::read_to_string("src/passwords.in")
+    let value = part_1();
+    println!("Part1: {}", value);
+
+    let value2 = part_2();
+    println!("Part2: {}", value2);
+}
+
+fn part_1() -> usize {
+    return fs::read_to_string("src/passwords.in")
         .unwrap()
         .lines()
-        .map(String::from)
         .filter(|s| is_password_compliant(s))
         .count();
+}
 
-    println!("{}", value);
+fn part_2() -> usize {
+    return fs::read_to_string("src/passwords.in")
+        .unwrap()
+        .lines()
+        .filter(|s| is_password_compliant_with_otcp(s))
+        .count();
 }
 
 fn is_password_compliant(line: &str) -> bool {
@@ -16,21 +29,40 @@ fn is_password_compliant(line: &str) -> bool {
         .map(String::from)
         .collect();
 
-    let mut p1 = splits.get(0)?.split("-");
-    let min = p1.next()?.parse::<usize>()?;
-    let max = p1.next()?.parse::<usize>()?;
+    let mut p1 = splits.get(0).unwrap().split("-");
+    let min = p1.next().unwrap().parse::<usize>().unwrap();
+    let max = p1.next().unwrap().parse::<usize>().unwrap();
 
-    let char = splits.get(1)?.chars().next()?;
-    let pwd = splits.get(2)?;
+    let char = splits.get(1).unwrap().chars().next().unwrap();
+    let pwd = splits.get(2).unwrap();
 
     let count = pwd.chars().filter(|c| *c == char).count();
 
     return min <= count && max >= count;
 }
 
+fn is_password_compliant_with_otcp(line: &str) -> bool {
+    let splits: Vec<String> = line.split_whitespace()
+        .map(String::from)
+        .collect();
+
+    let mut positions = splits.get(0).unwrap().split("-");
+    let p1 = positions.next().unwrap().parse::<usize>().unwrap();
+    let p2 = positions.next().unwrap().parse::<usize>().unwrap();
+
+    let char = splits.get(1).unwrap().chars().next().unwrap();
+    let pwd = splits.get(2).unwrap();
+
+    let matches_p1 = *pwd.chars().collect::<Vec<char>>().get(p1 - 1).unwrap() == char;
+    let matches_p2 = *pwd.chars().collect::<Vec<char>>().get(p2 - 1).unwrap() == char;
+
+    return matches_p1 ^ matches_p2;
+}
+
 #[cfg(test)]
 mod tests {
     use crate::is_password_compliant;
+    use crate::is_password_compliant_with_otcp;
 
     #[test]
     fn simple_test() {
@@ -45,5 +77,20 @@ mod tests {
     #[test]
     fn simple_test3() {
         assert_eq!(true, is_password_compliant("2-9 c: ccccccccc"))
+    }
+
+    #[test]
+    fn simple_test_v2_1() {
+        assert_eq!(true, is_password_compliant_with_otcp("1-3 a: abcde"))
+    }
+
+    #[test]
+    fn simple_test_v2_2() {
+        assert_eq!(false, is_password_compliant_with_otcp("1-3 b: cdefg"))
+    }
+
+    #[test]
+    fn simple_test_v2_3() {
+        assert_eq!(false, is_password_compliant_with_otcp("2-9 c: ccccccccc"))
     }
 }
