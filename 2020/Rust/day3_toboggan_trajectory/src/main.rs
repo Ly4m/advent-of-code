b89e42d25836b03c0acde0133c3035bdf12edbd6
William Leemans <will.leemans@gmail.com>
2020-12-03 21:09:29 +0100

    :sparkles: 2020 Day 3 part II

--- 2020/Rust/day3_toboggan_trajectory/src/main.rs
+++ 2020/Rust/day3_toboggan_trajectory/src/main.rs
@@ -5,26 +5,29 @@ fn main() {
         .map(String::from)
         .collect::<Vec<String>>();
 
-    let length = lines.get(0).unwrap().len();
-    let count = count_trees(lines, length);
+    let width = lines.get(0).unwrap().len();
+
+    let mut count = count_trees(&lines, width, 1, 1);
+    count *= count_trees(&lines, width, 1, 3);
+    count *= count_trees(&lines, width, 1, 5);
+    count *= count_trees(&lines, width, 1, 7);
+    count *= count_trees(&lines, width, 2, 1);
 
     println!("{}", count);
 }
 
-fn count_trees(lines: Vec<String>, length: usize) -> usize {
+fn count_trees(lines: &Vec<String>, width: usize, down_step:usize, right_step: usize) -> usize {
 
     let mut position  = 0;
 
     return lines
         .iter()
-        .skip(1)
+        .skip(down_step)
+        .step_by(down_step)
         .filter(|l| {
+            position += right_step; // move on x Axis
 
-            position += 3; // move on x Axis
-
-            if position >= length {  // if going out of bound
-                position -= length;
-            }
+            if position >= width { position -= width; } // prevent out of bound
 
             l.chars().nth(position).unwrap() == '#' // if tree
         }).count();
