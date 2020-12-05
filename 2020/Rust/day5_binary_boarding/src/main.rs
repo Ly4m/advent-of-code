a7cf4a0c371d98c137ce99b3c11ebb7f23639dcb
William Leemans <will.leemans@gmail.com>
2020-12-05 23:26:26 +0100

    :sparkles: 2020 Day 5 part II

--- 2020/Rust/day5_binary_boarding/src/main.rs
+++ 2020/Rust/day5_binary_boarding/src/main.rs
@@ -6,48 +6,75 @@ fn main() {
         .collect::<Vec<String>>();
 
     let highest_seat = find_highest_seat_id(&lines);
-    println!("{}", highest_seat)
+    println!("The highest seat ID {}", highest_seat);
+
+    let my_seat = find_missing_seat(&lines);
+    println!("My seat : {}", my_seat)
 }
 
-fn decode_boarding_pass(pass: &str) -> i32 {
-
-    let rows = &pass[0..7].replace('F',"0").replace("B", "1");
-    let columns = &pass[7..10].replace('L',"0").replace("R", "1");
+fn decode_boarding_pass(pass: &str) -> (i32, i32) {
+    let rows = &pass[0..7].replace('F', "0").replace("B", "1");
+    let columns = &pass[7..10].replace('L', "0").replace("R", "1");
 
     let seat_row = i32::from_str_radix(rows, 2).unwrap();
     let seat_column = i32::from_str_radix(columns, 2).unwrap();
 
+    (seat_row, seat_column)
+}
+
+fn compute_seat_id(seat_row: i32, seat_column: i32) -> i32 {
     (seat_row * 8) + seat_column
 }
 
 fn find_highest_seat_id(passes: &Vec<String>) -> i32 {
     passes.iter().map(|s| s.as_str())
         .map(|s| decode_boarding_pass(s))
+        .map(|s| compute_seat_id(s.0, s.1))
         .max().unwrap()
+}
+
+fn find_missing_seat(passes: &Vec<String>) -> i32 {
+    let mut all_seats = passes.iter().map(|s| s.as_str())
+        .map(|s| decode_boarding_pass(s))
+        .filter(|(row, column)| *row > 0 && *row < 127) // exclude first and last row
+        .map(|s| compute_seat_id(s.0, s.1))
+        .collect::<Vec<i32>>();
+
+    all_seats.sort();
+
+    *all_seats.iter()
+        .enumerate()
+        .find(|(index, seat)|  (*seat +1) != all_seats[index +1] )
+        .unwrap().1 + 1
 }
 
 
 #[cfg(test)]
 mod tests {
+    use crate::compute_seat_id;
     use crate::decode_boarding_pass;
 
     #[test]
     fn should_decode_simple_boarding_pass() {
-        assert_eq!(357, decode_boarding_pass("FBFBBFFRLR"))
+        let decoded_pass = decode_boarding_pass("FBFBBFFRLR");
+        assert_eq!(357, compute_seat_id(decoded_pass.0, decoded_pass.1));
     }
 
     #[test]
     fn should_decode_simple_boarding_pass2() {
-        assert_eq!(567, decode_boarding_pass("BFFFBBFRRR"))
+        let decoded_pass = decode_boarding_pass(("BFFFBBFRRR"));
+        assert_eq!(567, (compute_seat_id(decoded_pass.0, decoded_pass.1)))
     }
 
     #[test]
     fn should_decode_simple_boarding_pass3() {
-        assert_eq!(119, decode_boarding_pass("FFFBBBFRRR"))
+        let decoded_pass = decode_boarding_pass(("FFFBBBFRRR"));
+        assert_eq!(119, compute_seat_id(decoded_pass.0, decoded_pass.1))
     }
 
     #[test]
     fn should_decode_simple_boarding_pass4() {
-        assert_eq!(820, decode_boarding_pass("BBFFBBFRLL"))
+        let decoded_pass = decode_boarding_pass(("BBFFBBFRLL"));
+        assert_eq!(820, compute_seat_id(decoded_pass.0, decoded_pass.1))
     }
 }
